54808084cd2d9e9618f1e5155bbbe178cc2cd524
Frank Bell <fb@evilrobot.net>
2022-05-09 16:20:06 +0100

    Simple ABI deserialisation.

--- src/contracts/mod.rs
+++ src/contracts/mod.rs
@@ -1,7 +1,10 @@
 use super::{BoolFromStr, Result, ACTION, MODULE};
-use crate::ADDRESS;
-use serde::Deserialize;
+use crate::{APIError, ADDRESS};
+use serde::de::{value, IntoDeserializer};
+use serde::{Deserialize, Deserializer};
 use serde_with::{serde_as, DisplayFromStr};
+use std::str;
+use std::str::FromStr;
 
 #[cfg(test)]
 mod tests;
@@ -24,9 +27,10 @@ impl Client {
     /// # Arguments
     ///
     /// * 'address' - A contract address that has verified source code
-    pub async fn get_abi(&self, address: &str) -> Result<String> {
+    pub async fn get_abi(&self, address: &str) -> Result<Vec<Descriptor>> {
         let parameters = &[(MODULE, CONTRACT), (ACTION, "getabi"), (ADDRESS, address)];
-        self.client.get(parameters).await
+        let abi: String = self.client.get(parameters).await?;
+        serde_json::from_str(&abi).map_err(|e| APIError::DeserializationError { message: e.to_string() })
     }
 
     /// Returns the Solidity source code of a verified smart contract.
@@ -62,4 +66,110 @@ pub struct Contract {
     pub proxy: bool,
     pub implementation: String,
     pub swarm_source: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+#[serde(rename_all = "lowercase")]
+pub enum Descriptor {
+    Constructor {
+        inputs: Vec<Input>,
+    },
+    Function {
+        name: String,
+        inputs: Vec<Input>,
+        outputs: Vec<Output>,
+        constant: Option<bool>,
+        state_mutability: Option<StateMutability>,
+    },
+    Receive,
+    Fallback {
+        state_mutability: StateMutability,
+    },
+    Event {
+        name: String,
+        inputs: Vec<Input>,
+        anonymous: bool,
+    },
+    Error {
+        name: String,
+        inputs: Vec<Input>,
+    },
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Input {
+    name: String,
+    #[serde(rename = "type")]
+    input_type: Type,
+    components: Option<String>,
+    indexed: Option<bool>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Output {
+    name: String,
+    #[serde(rename = "type")]
+    output_type: Type,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum StateMutability {
+    // Does not read blockchain state
+    Pure,
+    // Does not modify blockchain state
+    View,
+    // Does not accept Ether
+    NonPayable,
+    // Accepts Ether
+    Payable,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+#[serde(remote = "Type")]
+pub enum Type {
+    Address,
+    #[serde(rename = "bool")]
+    Boolean,
+    Bytes,
+    Bytes32,
+    Fixed,
+    Int,
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+    Int128,
+    Int256,
+    UFixed,
+    UInt,
+    UInt8,
+    UInt16,
+    UInt32,
+    UInt64,
+    UInt126,
+    UInt256,
+    String,
+    Other(String),
+}
+
+impl FromStr for Type {
+    type Err = value::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Self::deserialize(s.into_deserializer())
+    }
+}
+
+impl<'de> Deserialize<'de> for Type {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let deserialized = Self::from_str(&s).unwrap_or_else(|_| Self::Other(s));
+        Ok(deserialized)
+    }
 }
--- src/contracts/tests.rs
+++ src/contracts/tests.rs
@@ -1,17 +1,27 @@
 use super::Client;
+use crate::contracts::Descriptor;
 use once_cell::sync::Lazy;
 
 const API_KEY: &str = "";
 const ADDRESS: &str = "0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413";
 const BURN_ADDRESS: &str = "0x0000000000000000000000000000000000000000";
+const ERC721_ADDRESS: &str = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
 
 static CLIENT: Lazy<Client> = Lazy::new(|| Client::new(API_KEY));
 
 #[tokio::test]
 async fn get_abi() -> Result<(), crate::APIError> {
     let abi = CLIENT.get_abi(ADDRESS).await?;
-    assert_ne!(0, abi.len());
-    println!("ABI of {} is {}", ADDRESS, abi);
+    assert!(abi.len() > 0);
+    println!("ABI of {} is \n{:#?}", ADDRESS, abi);
+    Ok(())
+}
+
+#[tokio::test]
+async fn get_abi_erc721() -> Result<(), crate::APIError> {
+    let abi = CLIENT.get_abi(ERC721_ADDRESS).await?;
+    assert!(abi.len() > 0);
+    println!("ABI of {} is \n{:#?}", ERC721_ADDRESS, abi);
     Ok(())
 }
 
@@ -78,6 +88,16 @@ async fn get_source_code() -> Result<(), crate::APIError> {
 
         assert_eq!(0, contract.swarm_source.len());
         println!("Swarm source:             {}", contract.swarm_source);
+    }
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn get_source_code_erc721() -> Result<(), crate::APIError> {
+    let contracts = CLIENT.get_source_code(ERC721_ADDRESS).await?;
+    for contract in contracts {
+        println!("Contract details for {ERC721_ADDRESS}:\n{:#?}", contract);
     }
 
     Ok(())
